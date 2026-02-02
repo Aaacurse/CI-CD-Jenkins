d24e3b792657e6324a3c6750112ef3af705aedde
Aaacurse <rajputaakarsh2004@gmail.com>
2026-02-02 18:25:00 +0530

    Pushing all files along with unit test

---
+++ src/operations.py
@@ -0,0 +1,7 @@
+def add(num1,num2):
+    sum=num1+num2
+    return sum
+
+def subtract(num1,num2):
+    diff=num1-num2
+    return diff
---
+++ test/test_operations.py
@@ -0,0 +1,12 @@
+from src.operations import add,subtract
+
+def test_add():
+    assert add(1,1)==2
+    assert add(-1,1)==0
+    assert add(-1,-1)==-2
+
+def test_subtract():
+    assert subtract(5,3)==2
+    assert subtract(2,2)==0
+    assert subtract(-1,-1)==0
+    assert subtract(3,5)==-2
